e92587435bf1de6313783fc17d86ad25009badec
Ok-michael <okomichaelakpu@gmail.com>
2023-11-28 23:05:15 +0100

    fix some semantics

--- 0x01-python-if_else_loops_functions/1-last_digit.py
+++ 0x01-python-if_else_loops_functions/1-last_digit.py
@@ -1,24 +1,15 @@
 #!/usr/bin/python3
 import random
 number = random.randint(-10000, 10000)
-
-# check for positivity
-if number < 0:
-    for_mod = number *- 1
-else:
-    for_mod = number
-
-# extract the last digit
-for_mod %= 10
+last_digit = abs(number) % 10
 
 if number < 0:
-    for_mod *= -1
+    last_digit = -last_digit
 
-# do the formal printing
-print("Last digit of", number, "is", (for_mod), end="")
-if (for_mod) > 5:
-    print(" and is greater than 5")
-elif (for_mod) == 0:
-    print(" and is 0")
-elif (for_mod < 6) and (for_mod != 0):
-    print(" and is less than 6 and not 0")
+if last_digit > 5:
+    print(f"Last digit of {number} is {last_digit} and is greater than 5")
+elif last_digit == 0:
+    print(f"Last digit of {number} is {last_digit} and is 0")
+elif last_digit < 6 and not 0:
+    print(f"Last digit of {number} is {last_digit} and \
+is less than 6 and not 0")
