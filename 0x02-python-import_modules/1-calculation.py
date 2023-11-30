f7037ea9aca13f8b0be6b2a9b776ed801780a632
Ok-michael <okomichaelakpu@gmail.com>
2023-11-30 17:30:39 +0100

    Write a program that prints the number of and the list of its arguments.

--- 0x02-python-import_modules/1-calculation.py
+++ 0x02-python-import_modules/1-calculation.py
@@ -1,10 +1,14 @@
 #!/usr/bin/python3
 
 if __name__ == "__main__":
-    import calculator_1
+    from calculator_1 import add
+    from calculator_1 import sub
+    from calculator_1 import mul
+    from calculator_1 import div
+
     a = 10
     b = 5
-    print("{:d} + {:d} = {:d}".format(a, b, calculator_1.add(a, b)))
-    print("{:d} - {:d} = {:d}".format(a, b, calculator_1.sub(a, b)))
-    print("{:d} * {:d} = {:d}".format(a, b, calculator_1.mul(a, b)))
-    print("{:d} / {:d} = {:d}".format(a, b, calculator_1.div(a, b)))
+    print("{:d} + {:d} = {:d}".format(a, b, add(a, b)))
+    print("{:d} - {:d} = {:d}".format(a, b, sub(a, b)))
+    print("{:d} * {:d} = {:d}".format(a, b, mul(a, b)))
+    print("{:d} / {:d} = {:d}".format(a, b, div(a, b)))
