12c9f0891bb0ce372804cd0f630ee85b6ca59306
Ok-michael <okomichaelakpu@gmail.com>
2023-12-04 23:21:35 +0100

    fix a semanti

--- 0x03-python-data_structures/13-is_palindrome.c
+++ 0x03-python-data_structures/13-is_palindrome.c
@@ -9,25 +9,25 @@
 
 int is_palindrome(listint_t **head)
 {
-    listint_t *cur = *head;
-    int tab[2048], i = 0, j = 0;
+	listint_t *cur = *head;
+	int tab[2048], i = 0, j = 0;
 
-    if (*head)
-    {
-        while (cur)
-        {
-            tab[i] = cur->n;
-            cur = cur->next;
-            i++;
-        }
+	if (*head)
+	{
+		while (cur)
+		{
+			tab[i] = cur->n;
+			cur = cur->next;
+			i++;
+		}
 
-        while (j < i / 2)
-        {
-            if (tab[j] == tab[i - j - 1])
-                j++;
-            else
-                return (0);
-        }
-    }
-    return (1);
+		while (j < i / 2)
+		{
+			if (tab[j] == tab[i - j - 1])
+				j++;
+			else
+				return (0);
+		}
+	}
+	return (1);
 }
