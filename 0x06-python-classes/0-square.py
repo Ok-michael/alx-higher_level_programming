f62a0a3d5ab36f78b5269cd4fd58223e142af053
Ok-michael <okomichaelakpu@gmail.com>
2023-12-19 14:59:08 +0100

    Write an empty class Square that defines a square

--- 0x06-python-classes/0-square.py
+++ 0x06-python-classes/0-square.py
@@ -1,11 +1,10 @@
 #!/usr/bin/python3
-"""
-    Class Square that defines a square.
+""" Class Square that defines a square.
 """
 
+
 class Square:
-    """
-        class Square - this is an empty class just to demonstrate
+    """ class Square - this is an empty class just to demonstrate
         how to creat empty classes
     """
 
