feee6df88dcd5ba62bd6cd625859ba6ca1429a37
Ok-michael <okomichaelakpu2@gmail.com>
2024-01-09 00:02:33 +0100

    make some modifications

--- 0x0A-python-inheritance/7-base_geometry.py
+++ 0x0A-python-inheritance/7-base_geometry.py
@@ -1,17 +1,18 @@
 #!/usr/bin/python3
-"""This module defines a class BaseGeometry"""
+"""
+Contains the class BaseGeometry
+"""
 
 
 class BaseGeometry:
-    """ this class defines a BaseGeometry with some methods """
-
+    """A class with public instance methods area and integer_validator"""
     def area(self):
-        """ this method simply raises an exception """
+        """raises an exception when called"""
         raise Exception("area() is not implemented")
 
     def integer_validator(self, name, value):
-        """ this method validates value """
-        if not isinstance(value, int):
+        """validates that value is an integer greater than 0"""
+        if type(value) is not int:
             raise TypeError(f"{name} must be an integer")
         if value <= 0:
             raise ValueError(f"{name} must be greater than 0")
--- 0x0A-python-inheritance/8-rectangle.py
+++ 0x0A-python-inheritance/8-rectangle.py
@@ -1,28 +1,16 @@
 #!/usr/bin/python3
-"""This module defines a class BaseGeometry"""
+"""
+Contains the class BaseGeometry and subclass Rectangle
+"""
 
-
-class BaseGeometry:
-    """ this class defines a BaseGeometry with some methods """
-
-    def area(self):
-        """ this method simply raises an exception """
-        raise Exception("area() is not implemented")
-
-    def integer_validator(self, name, value):
-        """ this method validates value """
-        if not isinstance(value, int):
-            raise TypeError(f"{name} must be an integer")
-        if value <= 0:
-            raise ValueError(f"{name} must be greater than 0")
+BaseGeometry = __import__('7-base_geometry').BaseGeometry
 
 
 class Rectangle(BaseGeometry):
-    """ this class extends BaseGeometry """
-
+    """A representation of a rectangle"""
     def __init__(self, width, height):
-        """ this is the constructor and it does the setups """
+        """instantiation of the rectangle"""
         self.integer_validator("width", width)
-        self.integer_validator("height", height)
         self.__width = width
+        self.integer_validator("height", height)
         self.__height = height
